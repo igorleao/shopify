3f0bd2cec0446dae610aad345be77bb4302b25ad
Igor Leao <ivpcl@cin.ufpe.br>
2016-11-15 02:26:15 -0300

    refactor(doc, name): slightly improve names and comments

--- shopicruit_clocks_price.rb
+++ shopicruit_clocks_price.rb
@@ -1,13 +1,13 @@
 ##
 # This script retrieves all Cloks from shopicruit.myshopify.com public API
-# and calculates its sum
+# and calculates how much it would cost to buy all of them
 #
 # Requirements: 1. Network connection
 # =>            2. Endpoint http://shopicruit.myshopify.com/products.json alive
-# =>            3. Same data structure as seen on Nov 14, 2016 (returned by the endpoint)
+# =>            3. Same data structure returned by the endpoint as seen on Nov 14, 2016
 # Input:        None
 # Output:       sum of the prices of clocks available at shopicruit.myshopify.com
-# =>            -> It return the sum from clocks holding any SKU (actually, all SKU are = "")
+#               (any S.K.U.)
 #
 # PS1: IN ORDER TO AVOID INCREASING COMPLEXITY (UNLESS IT'S NECESSARY),
 # I CONCEIVED THIS APPLICATION AS A HUMBLE SCRIPT, NOT STRUCTURING IT THROUGH CLASSES
@@ -17,7 +17,7 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-# URL OF THE STORE
+# URL TO THE STORE
 SHOPICRUIT_URL = 'http://shopicruit.myshopify.com/products.json'.freeze
 
 # SOME FIELDS AT THE RESPONSE OF A GET REQUEST FOR THE ENDPOINT ABOVE
@@ -31,11 +31,11 @@ PRICE_VARIANT_FIELD = 'price'.freeze
 # Section which creates generic helper functions to handle Network
 
 ##
-# Create a URI given a URL and some params
+# Create a URI given a URL(string) and some params
 # Params: URL -> base URL of this endpoint
 # =>      params -> params to be attached to the given URL
 # Return: The new URI
-def self.create_uri(url, parms)
+def create_uri(url, parms)
     uri = URI(url)
     uri.query = URI.encode_www_form(parms)
     uri
@@ -45,7 +45,7 @@ end
 # Create a HTTP/GET request and retrieve its content
 # Params: uri -> URI which the request is going to be made
 # Return: Either the body of the request or nil (if the request doesn't succeed)
-def self.make_get_request(uri)
+def make_get_request(uri)
     response = Net::HTTP.get_response(uri)
     response.is_a?(Net::HTTPSuccess) ? response.body : nil
 end
@@ -59,7 +59,7 @@ end
 # =>      field -> a field at the given item
 # =>      value -> a value to be filtered
 # Return: True if the given item holds the given field and value and False otherwise
-def self.filter_item_by_type(item, field, value)
+def filter_item_by_type(item, field, value)
     item[field] == value
 end
 
@@ -74,13 +74,13 @@ end
 # =>        retrived by all previous requests
 # Params: None
 # Return: List containing all products at shopicruit.myshopify.com
-def self.all_products
+def all_products
     params = { page: 0 }
     products = []
     loop do
         params[:page] += 1
-            uri = self.create_uri(SHOPICRUIT_URL, params)
-            response_body = self.make_get_request(uri)
+            uri = create_uri(SHOPICRUIT_URL, params)
+            response_body = make_get_request(uri)
             current_products = response_body.nil? ? [] : JSON.parse(response_body)[PRODUCTS_FIELD]
             products.concat(current_products)
         break if current_products.empty?
@@ -92,15 +92,15 @@ end
 # Retrive all products from shopicruit.myshopify.com holding a given type
 # Params:   type -> type of the desired product
 # Return: List containing all products at shopicruit.myshopify.com holding a given type
-def self.products_by_type(type)
-    self.all_products.select { |product| self.filter_item_by_type(product, TARGET_PRODUCT_FIELD, type) }
+def products_by_type(type)
+    all_products.select { |product| filter_item_by_type(product, TARGET_PRODUCT_FIELD, type) }
 end
 
 ##
 # Calculate the sum of the prices for a list of products (considering its variants)
 # Params:   products -> list containg products from a certain Shopify store
 # Return: Total price for the given products with 2 decimals
-def self.sum_price(products)
+def sum_prices(products)
     sum = 0.0
     products.each do |product|
         product[VARIANTS_FIELD].each do |variant_product|
@@ -111,7 +111,7 @@ def self.sum_price(products)
 end
 
 ##
-# RUN THE SCRIPT :)
-all_products = self.products_by_type(CLOCK_TYPE)
+# RUN SCRIPT :)
+clocks_only = products_by_type(CLOCK_TYPE)
 # WRITE RESULT TO STDOUT
-puts self.sum_price(all_products)
+puts sum_prices(clocks_only)
